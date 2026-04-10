aca12dfc3549ffcbff861555a5e0e308b990405e
Israamanaziz <azizisra2001@gmail.com>
2026-04-10 21:56:29 +0530

    Commit by IAA

--- pages/life_event_marriage.py
+++ pages/life_event_marriage.py
@@ -1,2 +1,52 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
+st.title("💍 Wedding | SofA")
+
+st.divider()
+
+st.header("Matrimonial sites")
+
+"""
+- **[Shaadi.com](https://www.shaadi.com/)** 
+- **[BharatMatrimony.com](https://www.bharatmatrimony.com/)**
+- **[Jeevansathi.com](https://www.jeevansathi.com/)**
+- **Newspaper Advertisements**: Most ads are published in the **Sunday Edition**.
+    - **[Times Soulmate (TOI)](https://ads.timesofindia.com/):** The highest reach in English-speaking urban India. Look for their 'Buy 2 Get 2' deals.
+    - **[HT Classifieds](https://www.htclassifieds.com/):** For Delhi, NCR, and North India.
+    - **[The Hindu](https://thehinduads.com/):** 
+
+"""
+
+st.header("Professional & Education specific sites")
+"""
+- **[IIMIITMatrimony](https://www.iimiitmatrimony.com/):** Specifically for graduates of premier institutions (IITs, IIMs, and other top-tier colleges).
+- **[Elite Matrimony](https://www.elitematrimony.com/):** A premium, invite-only service for affluent families and top-ranking professionals.
+- **[Betterhalf.ai](https://www.betterhalf.ai/):** A modern approach focusing on 'compatibility first,' popular with the urban corporate crowd.
+"""
+
+st.header("Cultural & Community Specific sites")
+"""
+- **[Community Matrimony](https://www.communitymatrimony.com/):** The parent site for thousands of micro-sites dedicated to specific castes and sub-castes (e.g., Tamil, Marathi, Agarwal, etc.).
+- **Religion-Focused:**
+    - **[Muslim Matrimony](https://www.muslimmatrimony.com/)** 
+    - **[Christian Matrimony](https://www.christianmatrimony.com/)** 
+    - **[Sikh Matrimony](https://www.sikhmatrimony.com/)** 
+"""
+
+st.divider()
+
+st.header("Safety Tips")
+
+"""
+- **No matter how legit the site is, individual users can be unpredictable, so use the sites with caution.**
+- **Verify the ID:** Only interact with profiles that have a 'Verified' or 'ID-Checked' badge. 
+- **Money is a Red Flag:** If a match asks for money for any reason (emergencies, customs, gifts), it is a scam. Block them instantly.
+- **Privacy First:** Use the in-app calling features provided by sites like Jeevansathi or Shaadi to avoid giving out your personal phone number too early.
+- **Public Meetings:** Always choose a busy cafe or mall for your first few meetings. Inform a friend or family member where you are going.
+"""
+
+st.divider()
+
+
